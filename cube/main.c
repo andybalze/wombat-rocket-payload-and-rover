eef9a3530267e4c0359b67121c80717e4f0449e2
Jordan Johnson <a02293752@usu.edu>
2024-01-16 19:05:24 -0700

    barebones framework

---
+++ cube/main.c
@@ -0,0 +1,16 @@
+/* * * * * * * * * * * * * * *
+      Data Cube Software
+ * * * * * * * * * * * * * * */
+
+#ifndef F_CPU
+#define F_CPU 8000000 //8 MHz
+#endif
+
+int main() {
+
+	// Setup
+
+	// Loop
+	while(1);
+
+}
---
+++ rover/main.c
@@ -0,0 +1,16 @@
+/* * * * * * * * * * * * * * *
+         Rover Software
+ * * * * * * * * * * * * * * */
+
+#ifndef F_CPU
+#define F_CPU 8000000 //8 MHz
+#endif
+
+int main() {
+
+	// Setup
+
+	// Loop
+	while(1);
+
+}
