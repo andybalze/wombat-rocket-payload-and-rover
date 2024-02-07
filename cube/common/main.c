65f30b8cd5a602ffc0a5ffd03379d67df56f6a27
kmskillings <kmskillings@gmail.com>
2024-02-07 00:43:47 -0700

    LED is controlled by two interrupts.

--- cube/common/main.c
+++ cube/common/main.c
@@ -11,18 +11,50 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <avr/io.h>
-#include <avr/interrupt.h>>
+#include <avr/interrupt.h>
 
 int main(void) {
 
   // Configure the chosen pin as an output.
   DDRB |= _BV(DDB0);
+
+  // Configure Timer1 to generate interrupts.
   
+  // Selects the 1/1024 prescaler.
+  TCCR1B |= _BV(CS10);
+
+  // Sets the timer into CTC mode.
+  TCCR1B |= _BV(WGM12);
+
+  // Sets the output compare value for turning off the LED.
+  OCR1A = 15624; // 16,000,000/1024 - 1 = 15,624
+
+  // Sets the output compare value for turning on the LED.
+  // The LED turns on halfway through the counter cycle.
+  OCR1B = 7812;
+
+  // Enables the output compare A and B interrupts
+  TIMSK1 |= _BV(OCIE1A);
+  TIMSK1 |= _BV(OCIE1B);
+
+  // Enables global interrupts.
+  SREG |= _BV(SREG_I);
+
+  // Loops forever at the end of main.
+  while(1);
+
 }
 
-ISR(TIMER0_COMPA_vect) {
+ISR(TIMER1_COMPB_vect) {
 
-  // Toggles the LED state.
-  PORTB ^= _BV(PORTB0);
+  // Turns on the LED.
+  PORTB |= _BV(PORTB0);
+
+}
+
+ISR(TIMER1_COMPA_vect) {
+
+  // Turns off the LED.
+  PORTB &= ~_BV(PORTB0);
 
 }
