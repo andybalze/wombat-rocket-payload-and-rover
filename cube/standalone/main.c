aa232594c9df1219b301ae2936a1e50804925554
kmskillings <kmskillings@gmail.com>
2024-02-08 01:21:42 -0700

    Created simple test main for uart functionality.

--- cube/standalone/main.c
+++ cube/standalone/main.c
@@ -2,15 +2,28 @@
       Data Cube Software
  * * * * * * * * * * * * * * */
 
-#ifndef F_CPU
-#define F_CPU 8000000 //8 MHz
-#endif
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "uart.h"
+
+static uart_message_element_t received_data;
+
+static char* restart_message = "Hello World.\n\r";
+static int restart_message_length = 14;
 
 int main() {
 
-	// Setup
+	uart_initialize();
 
-	// Loop
+	uart_transmit_message(restart_message, restart_message_length);
+
 	while(1);
 
+}
+
+// Echo any message received.
+ISR(USART_RX_vect) {
+	received_data = UDR0;
+	uart_transmit_message(&received_data, 1);
 }
