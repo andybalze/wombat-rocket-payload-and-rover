4006061d759d1a10af3e6c775545ae6021e283bb
kmskillings <kmskillings@gmail.com>
2024-02-20 22:30:31 -0700

    Fleshed out ADC register manipulation

--- rover/adc.c
+++ rover/adc.c
@@ -18,13 +18,13 @@
 #define ADC_REFS_INTERNAL ( _BV(REFS0) | _BV(REFS1) )
 #define ADC_REFS_MASK     ( _BV(REFS0) | _BV(REFS1) )
 #if ADC_REFERENCE_MODE == ADC_REFERENCE_MODE_AREF
-  #define ADC_REFS ADC_REFS_AREF
+  #define ADC_REFS (ADC_REFS_AREF & ADC_REFS_MASK)
 #elif ADC_REFERENCE_MODE == ADC_REFERENCE_MODE_AVCC
-  #define ADC_REFS ADC_REFS_AVCC
+  #define ADC_REFS (ADC_REFS_AVCC & ADC_REFS_MASK)
 #elif ADC_REFERENCE_MODE == ADC_REFERENCE_MODE_INTERNAL
-  #define ADC_REFS ADC_REFS_INTERNAL
+  #define ADC_REFS (ADC_REFS_INTERNAL & ADC_REFS_MASK)
 #else
-  #define ADC_REFS ADC_REFS_INTERNAL
+  #define ADC_REFS (ADC_REFS_INTERNAL & ADC_REFS_MASK)
 #endif
 
 #define ADC_ADPS_2    (0)
@@ -33,24 +33,24 @@
 #define ADC_ADPS_16   (                           _BV(ADPS2)  )
 #define ADC_ADPS_32   ( _BV(ADPS0)              | _BV(ADPS2)  )
 #define ADC_ADPS_64   (              _BV(ADPS1) | _BV(ADPS2)  )
-#define ADC_ADPS_128  ( _BV(ADPS0)   _BV(ADPS1) | _BV(ADPS2)  )
-#define ADC_ADPS_MASK ( _BV(ADPS0)   _BV(ADPS1) | _BV(ADPS2)  )
+#define ADC_ADPS_128  ( _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2)  )
+#define ADC_ADPS_MASK ( _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2)  )
 #if ADC_PRESCALER == 2
-  #define ADC_ADPS ADC_ADPS_2
+  #define ADC_ADPS (ADC_ADPS_2 & ADC_ADPS_MASK)
 #elif ADC_PRESCALER == 4
-  #define ADC_ADPS ADC_ADPS_4
+  #define ADC_ADPS (ADC_ADPS_4 & ADC_ADPS_MASK)
 #elif ADC_PRESCALER == 8
-  #define ADC_ADPS ADC_ADPS_8
+  #define ADC_ADPS (ADC_ADPS_8 & ADC_ADPS_MASK)
 #elif ADC_PRESCALER == 16
-  #define ADC_ADPS ADC_ADPS_16
+  #define ADC_ADPS (ADC_ADPS_16 & ADC_ADPS_MASK)
 #elif ADC_PRESCALER == 32
-  #define ADC_ADPS ADC_ADPS_32
+  #define ADC_ADPS (ADC_ADPS_32 & ADC_ADPS_MASK)
 #elif ADC_PRESCALER == 64
-  #define ADC_ADPS ADC_ADPS_64
+  #define ADC_ADPS (ADC_ADPS_64 & ADC_ADPS_MASK)
 #elif ADC_PRESCALER == 128
-  #define ADC_ADPS ADC_ADPS_128
+  #define ADC_ADPS (ADC_ADPS_128 & ADC_ADPS_MASK)
 #else
-  #define ADC_ADPS ADC_ADPS_128
+  #define ADC_ADPS (ADC_ADPS_128 & ADC_ADPS_MASK)
 #endif
 
 #define ADC_CHANNEL_COUNT (11)
@@ -58,7 +58,7 @@
 #define ADC_MUX_ADC1 (  _BV(MUX0)                                     )
 #define ADC_MUX_ADC2 (              _BV(MUX1)                         )
 #define ADC_MUX_ADC3 (  _BV(MUX0) | _BV(MUX1)                         )
-#define ADC_MUX_ADC4 (                        | _BV(MUX2)             )
+#define ADC_MUX_ADC4 (                          _BV(MUX2)             )
 #define ADC_MUX_ADC5 (  _BV(MUX0)             | _BV(MUX2)             )
 #define ADC_MUX_ADC6 (              _BV(MUX1) | _BV(MUX2)             )
 #define ADC_MUX_ADC7 (  _BV(MUX0) | _BV(MUX1) | _BV(MUX2)             )
@@ -85,6 +85,48 @@
 #define ADC_CHANNEL_COUNT (11)
 static adc_result_t adc_results_buffer[ADC_CHANNEL_COUNT];
 
+// The sequence of MUX values to rotate through. Includes only the enabled
+// channels.
+static char adc_mux_sequence[] = {
+#ifdef ADC_ENABLE_ADC0
+  ADC_MUX_ADC0,
+#endif
+#ifdef ADC_ENABLE_ADC1
+  ADC_MUX_ADC1,
+#endif
+#ifdef ADC_ENABLE_ADC2
+  ADC_MUX_ADC2,
+#endif
+#ifdef ADC_ENABLE_ADC3
+  ADC_MUX_ADC3,
+#endif
+#ifdef ADC_ENABLE_ADC4
+  ADC_MUX_ADC4,
+#endif
+#ifdef ADC_ENABLE_ADC5
+  ADC_MUX_ADC5,
+#endif
+#ifdef ADC_ENABLE_ADC6
+  ADC_MUX_ADC6,
+#endif
+#ifdef ADC_ENABLE_ADC7
+  ADC_MUX_ADC7,
+#endif
+#ifdef ADC_ENABLE_TEMP
+  ADC_MUX_TEMP,
+#endif
+#ifdef ADC_ENABLE_REF
+  ADC_MUX_REF,
+#endif
+#ifdef ADC_ENABLE_GND
+  ADC_MUX_GND,
+#endif
+};
+int adc_mux_sequence_length = sizeof(adc_mux_sequence) / sizeof(*adc_mux_sequence);
+
+// Tracks what the next ADC channel to be sampled is.
+int adc_mux_sequence_next_index;
+
 //////////////// Public Function Bodies ////////////////////////////////////////
 
 // Initializes the ADC, including configuring the appropriate pins.
@@ -95,6 +137,8 @@ void adc_initialize(void) {
   for (i = 0; i < ADC_CHANNEL_COUNT; i++) {
     adc_results_buffer[i] = 0;
   }
+
+  adc_mux_sequence_next_index = 0;
 
   // Set up ADC registers
 
@@ -108,8 +152,31 @@ void adc_initialize(void) {
   ADCSRA = (
     _BV(ADEN) | // Turns on the ADC.
     _BV(ADIE) | // Enables the conversion complete interrupt.
-    ADC_ADPS  | // Sets the 
-  )
+    ADC_ADPS    // Sets the ADC clock prescaler.
+  );
+
+  // Enable or disable digital inputs for appropriate pins.
+  DIDR0 = (
+    0
+    #ifdef ADC_ENABLE_ADC0
+      | _BV(ADC0D)
+    #endif
+    #ifdef ADC_ENABLE_ADC1
+      | _BV(ADC1D)
+    #endif
+    #ifdef ADC_ENABLE_ADC2
+      | _BV(ADC2D)
+    #endif
+    #ifdef ADC_ENABLE_ADC3
+      | _BV(ADC3D)
+    #endif
+    #ifdef ADC_ENABLE_ADC4
+      | _BV(ADC4D)
+    #endif
+    #ifdef ADC_ENABLE_ADC5
+      | _BV(ADC5D)
+    #endif
+  );
 
 }
 
