d59c48ce6bc94ec5cbff615139899e7186cd501d
kmskillings <kmskillings@gmail.com>
2024-02-26 22:06:22 -0700

    Created absolute bare minimum program

--- cube/standalone/main.c
+++ cube/standalone/main.c
@@ -14,53 +14,9 @@
 #include <avr/interrupt.h>
 
 int main(void) {
-
-  // Configure the chosen pin as an output.
-  DDRB |= _BV(DDB0);
-
-  // Configure Timer1 to generate interrupts.
   
-  // Selects the 1/1024 prescaler.
-  TCCR1B |= _BV(CS10);
-
-  // Sets the timer into CTC mode.
-  TCCR1B |= _BV(WGM12);
-
-  // Sets the output compare value for turning off the LED.
-  OCR1A = 7813; // 8,000,000/1024 - 1 = 7,813
-
-  // Sets the output compare value for turning on the LED.
-  // The LED turns on halfway through the counter cycle.
-  OCR1B = 3906;
-
-  // Enables the output compare A and B interrupts
-  TIMSK1 |= _BV(OCIE1A);
-  TIMSK1 |= _BV(OCIE1B);
-
-  // Enables global interrupts.
-  SREG |= _BV(SREG_I);
-
-  // Loops forever at the end of main.
+  DDRB |= (_BV(DDB0));
+  PORTB &= ~(_BV(PORTB0));
+  
   while(1);
-
-}
-
-// Handles the interrupt generated whenever the count register of Timer 1
-// equals the value in register OCR1B. In this case, that means turning the
-// LED on.
-ISR(TIMER1_COMPB_vect) {
-
-  // Turns on the LED.
-  PORTB |= _BV(PORTB0);
-
-}
-
-// Handles the interrupt generated whenever the count register of Timer 1
-// equals the value in register OCR1A. In this case, that means turning the
-// LED off.
-ISR(TIMER1_COMPA_vect) {
-
-  // Turns off the LED.
-  PORTB &= ~_BV(PORTB0);
-
 }
