dfdb9f5188cae411b728cc5bb2d2013bcc6a58fd
kmskillings <kmskillings@gmail.com>
2024-03-20 11:35:57 -0600

    Drafted test program for SPI.

--- cube/standalone/main.c
+++ cube/standalone/main.c
@@ -6,24 +6,64 @@
 #include <avr/interrupt.h>
 
 #include "uart.h"
+#include "spi.h"
 
 static uart_message_element_t received_data;
 
-static char* restart_message = "Hello World.\n\r";
-static int restart_message_length = 14;
+static char* restart_message_format 					= "Hello World.\n\r";
+static char* spi_initialized_message_format 	= "SPI perhipheral initialized.\n\r";
+static char* spi_echo_message_format 					= "Received message over SPI: %02x%02x%02x%02x";
+
+// "DEADBEEF", an easily recognizeable hex number.
+static spi_message_element_t spi_message[] = {0xDE, 0xAD, 0xBE, 0xEF};
+static int spi_message_length = 4;
+
+void echo_spi_received(
+	const spi_message_element_t *received_message,
+	int received_message_length
+);
 
 int main() {
 
 	uart_initialize();
+	uart_transmit_formatted_message(restart_message_format);
+	
+	// Wait until the message is done being transmitted.
+	while((UCSR0B & _BV(UDRIE0)) != 0);
+	// TODO: Make a macro or function to replace this?
 
-	uart_transmit_message(restart_message, restart_message_length);
+	spi_initialize();
+	uart_transmit_formatted_message(spi_initialized_message_format);
+
+	// Wait until the message is done being transmitted.
+	while((UCSR0B & _BV(UDRIE0)) != 0);
+	// TODO: Make a macro or function to replace this?
+
+	spi_begin_transaction(
+		spi_message,
+		spi_message_length,
+		echo_spi_received
+	);
 
 	while(1);
 
 }
 
-// Echo any message received.
-ISR(USART_RX_vect) {
-	received_data = UDR0;
-	uart_transmit_message(&received_data, 1);
+void echo_spi_received(
+	const spi_message_element_t *received_message,
+	int received_message_length
+) {
+
+	uart_transmit_formatted_message(
+		spi_echo_message_format,
+		received_message[0],
+		received_message[1],
+		received_message[2],
+		received_message[3]
+	);
+
+	// Wait until the message is done being transmitted.
+	while((UCSR0B & _BV(UDRIE0)) != 0);
+	// TODO: Make a macro or function to replace this?
+
 }
