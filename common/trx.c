28aa494afc191f051362465617d022793b967298
kmskillings <kmskillings@gmail.com>
2024-03-26 19:23:57 -0600

    Demo reading and writing transceiver registers.

--- common/trx.c
+++ common/trx.c
@@ -17,8 +17,12 @@
 #include "trx.h"
 
 #include "spi.h"
+#include "uart.h"
 
 /////////////////// Private Defines ////////////////////////////////////////////
+
+#define TRX_TRANSACTION_MAX_LENGTH  (33)
+// 1 for the instruction, up to 32 for the data.
 
 // Transceiver instruction definitions.
 
@@ -57,9 +61,16 @@
 
 trx_status_buffer_t trx_status_buffer;
 
+spi_message_element_t trx_next_spi_message_buffer[TRX_TRANSACTION_MAX_LENGTH]; 
+
 /////////////////// Private Function Prototypes ////////////////////////////////
 
 void update_status_buffer (
+  const spi_message_element_t *received_message,
+  int received_message_length
+);
+
+void uart_transmit_register_value(
   const spi_message_element_t *received_message,
   int received_message_length
 );
@@ -69,6 +80,14 @@ void update_status_buffer (
 // Initializes the TRX, including initializing the SPI and any other peripherals
 // required.
 void trx_initialize() {
+
+  volatile int ready_to_move_on = 0;
+
+  // Sets up static variables.
+  /*int i;
+  for (i = 0; i < TRX_TRANSACTION_MAX_LENGTH; i++) {
+    trx_next_spi_message_buffer[i] = 0;
+  }*/
 
   // Sets up GPIO pins used to drive transceiver.
 
@@ -80,17 +99,52 @@ void trx_initialize() {
   spi_initialize();
 
   // Send a no-op over the SPI in order to update the trx status buffer.
-  spi_message_element_t instruction;
-  instruction = TRX_NOOP_INSTRUCTION;
+  trx_next_spi_message_buffer[0] = TRX_NOOP_INSTRUCTION;
   spi_begin_transaction(
-    &instruction,
+    trx_next_spi_message_buffer,
     1,
     update_status_buffer
   );
+  SPI_WAIT_UNTIL_DONE();
 
-  // Wait until the SPI message has finished sending.
-  while ((SPCR & _BV(SPIE)) != 0);
+  // Read the address-width register.
+  trx_next_spi_message_buffer[0] = 
+    TRX_READ_REGISTER_INSTRUCTION |
+    (TRX_REGISTER_ADDRESS_SETUP_AW & TRX_READ_REGISTER_ADDRESS_MASK)
+  ;
+  spi_begin_transaction(
+    trx_next_spi_message_buffer,
+    2,
+    uart_transmit_register_value
+  );
+  SPI_WAIT_UNTIL_DONE();
+  UART_WAIT_UNTIL_DONE();
 
+  // Write the address-width register.
+  trx_next_spi_message_buffer[0] = 
+    TRX_WRITE_REGISTER_INSTRUCTION |
+    (TRX_REGISTER_ADDRESS_SETUP_AW & TRX_READ_REGISTER_ADDRESS_MASK)
+  ;
+  trx_next_spi_message_buffer[1] = TRX_SETUP_AW;
+  spi_begin_transaction(
+    trx_next_spi_message_buffer,
+    2,
+    NULL
+  );
+  SPI_WAIT_UNTIL_DONE();
+
+  // Read the address-width register.
+  trx_next_spi_message_buffer[0] = 
+    TRX_READ_REGISTER_INSTRUCTION |
+    (TRX_REGISTER_ADDRESS_SETUP_AW & TRX_READ_REGISTER_ADDRESS_MASK)
+  ;
+  spi_begin_transaction(
+    trx_next_spi_message_buffer,
+    2,
+    uart_transmit_register_value
+  );
+  SPI_WAIT_UNTIL_DONE();
+  UART_WAIT_UNTIL_DONE();
 }
 
 void trx_transmit_payload(
@@ -116,4 +170,14 @@ void update_status_buffer(
   int received_message_length
 ) {
   trx_status_buffer = received_message[0];
+}
+
+void uart_transmit_register_value(
+  const spi_message_element_t *received_message,
+  int received_message_length
+) {
+  uart_transmit_formatted_message(
+    "Register value: %02x\n\r",
+    received_message[1]
+  );
 }
