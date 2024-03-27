59704530fb61b2e39099a80125bab061d24c8828
kmskillings <kmskillings@gmail.com>
2024-03-26 19:23:38 -0600

    Removed debug statements from main.

--- cube/standalone/main.c
+++ cube/standalone/main.c
@@ -18,19 +18,10 @@ int main() {
 
 	uart_initialize();
 	uart_transmit_formatted_message(restart_message_format);
-	
-	// Wait until the message is done being transmitted.
-	while((UCSR0B & _BV(UDRIE0)) != 0);
-	// TODO: Make a macro or function to replace this?
+	UART_WAIT_UNTIL_DONE();
 
 	trx_initialize();
 	// As part of this function, the status is retrieved from the transceiver.
-	
-	// Wait until the SPI transaction finishes.
-	while((SPCR & _BV(SPIE)) != 0);
-
-	spi_message_element_t status = trx_get_status();
-	uart_transmit_formatted_message(status_message_format, status);
 
 	while(1);
 
