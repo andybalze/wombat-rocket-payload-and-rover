cf2c8fe7a83e11b2b3db241b819f58f4fdda133b
Mitchell <allosaurus511@gmail.com>
2024-04-10 20:35:47 -0600

    Adjusted equation for type conversion.

--- rover/ir.c
+++ rover/ir.c
@@ -21,13 +21,16 @@
 int16_t ir_distance_read(void) {
 
     // Define output type
-    int16_t ir_distance_cm, ir_distance_adc;
+    signed int ir_distance_adc, distance;
+    int16_t ir_distance_cm;
 
     // Request ADC value of IR distance sensor
-    ir_distance_adc = adc_get_channel_result(ADC_CHANNEL_ADC0);
+    ir_distance_adc = (signed int) adc_get_channel_result(ADC_CHANNEL_ADC0);
+
+    distance = 61 - (ir_distance_adc/12);
 
     // ADC value to centimeters equation (Value will be truncated )
-    ir_distance_cm = -(ir_distance_adc/12)+61;  // Verified division works properly, when negative is added it sets to 2's complement, when added the adc read value goes to zero...
+    ir_distance_cm = (int16_t) distance;  // Verified division works properly, when negative is added it sets to 2's complement, when added the adc read value goes to zero...
 
     // Return distance in centimeters
     return ir_distance_cm;
