9f6962fad6455d0edb1f2130b660562dce69bcce
auger <austinb.gerber@gmail.com>
2024-04-19 18:22:58 -0600

    Wrote rover launch check code

--- rover/accelerometer.c
+++ rover/accelerometer.c
@@ -16,16 +16,10 @@
 #include "adc.h"
 #include "timer.h"
 
-// Don't use ONE_G macro too freely! You'll introduce big rounding errors!
-// 1 G is actually 19.6 (1/2)*m/(s^2). I rounded down so is_launched is slightly more sensitive.
-#define ONE_G           19          // Use with caution
 
-#define LAUNCH_FORCE    8 * ONE_G
+///////////////////// Global Variables /////////////////////////////////////////
+volatile bool launch_is_a_go = false;
 
-// Sample every 0.1 second for 10 seconds        // TEST // Get a second, third, fourth opinion
-#define LAUNCH_SAMPLE_RATE      100
-#define LAUNCH_SAMPLE_NUM       100
-#define NUM_SAMPLES_ACCEPTABLE  95
 
 ///////////////////// Public Function Prototypes ///////////////////////////////
 
@@ -53,16 +47,17 @@ int16_t accelerometer_read(char axis) {
     return accel_returned;
 }
 
-// Returns magnitude of agragate vector in (1/2)*m/(s^2)
-uint16_t acceleration_agg_mag(void) {
-    uint16_t x_comp, y_comp, z_comp;
-    int16_t mag_returned;
+// Returns magnitude of aggregate vector in (1/2)*m/(s^2)
+uint32_t acceleration_agg_mag(void) {
+    int32_t x_comp, y_comp, z_comp;
+    uint32_t mag_returned;
 
     x_comp = accelerometer_read(X_AXIS);
     y_comp = accelerometer_read(Y_AXIS);
     z_comp = accelerometer_read(Z_AXIS);
 
-    mag_returned = sqrt(pow(x_comp, 2) + pow(y_comp, 2) + pow(z_comp, 2));  // Good old Pythagoreas
+    // mag_returned = sqrt(pow(x_comp, 2) + pow(y_comp, 2) + pow(z_comp, 2));  // Good old Pythagoreas
+    mag_returned = (x_comp * x_comp) + (y_comp * y_comp) + (z_comp * z_comp);
 
     return mag_returned;
 }
@@ -82,31 +77,20 @@ bool is_up(void) {
     return return_val;
 }
 
-// Returns false if the rocket has not launched. Returns true if the rocket has launched.
-// Does not enter function for a substantial amount of time if there are high Gs
-bool is_launched(void) {
-    uint16_t gamma;         // letter scramble of acc_agg_mag
-    uint8_t  high_G_sample_cnt = 0;
-    bool return_val = false;
-
-    gamma = acceleration_agg_mag();
-
-    if (gamma > LAUNCH_FORCE) {
-        for (int i = 0; i < LAUNCH_SAMPLE_NUM; i++) {
-            reset_timer(timer_beta);
-            while (get_timer_cnt(timer_beta) < LAUNCH_SAMPLE_RATE);
-
-            gamma = acceleration_agg_mag();
-
-            if (gamma > LAUNCH_FORCE) {
-                high_G_sample_cnt++;
-            }
+// Called by timer0 channel B interrupt. Changes launch_is_a_go global variable
+void is_launched(uint64_t samples) {
+    uint64_t check_vector = 0x1;
+    uint8_t high_G_cnt = 0;
+    
+    for (int i = 0; i < 64; i++) {
+        if (samples & check_vector) {
+            high_G_cnt++;
         }
 
-        if (high_G_sample_cnt >= NUM_SAMPLES_ACCEPTABLE) {
-            return_val = true;
-        }
+        check_vector = check_vector << 1;
     }
 
-    return return_val;
+    if (check_vector >= LAUNCH_FORCE_CNT_THRESHOLD) {
+        launch_is_a_go = true;
+    }
 }
--- rover/accelerometer.h
+++ rover/accelerometer.h
@@ -10,10 +10,27 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
 #include <stdbool.h>
+
+
+///////////////////// Macro Definitions /////////////////////////////////////////
+
+// Don't use ONE_G macro too freely! You'll introduce big rounding errors!
+// 1 G is actually 19.6 (1/2)*m/(s^2). I rounded down so is_launched is slightly more sensitive.
+// #define ONE_G                   20          // Use with caution
+#define ONE_G_SQUARED           385         // Also use with caution
+
+#define LAUNCH_FORCE            3           // Over 3 G at launch. This is the thing you change if you want to adjust rocket launch sensing sensitivity
+#define LAUNCH_FORCE_SQUARED    LAUNCH_FORCE * LAUNCH_FORCE * ONE_G_SQUARED
+
+#define LAUNCH_FORCE_CNT_THRESHOLD 58       // 58 is 90% of 64 samples
+
+
+
 
 ///////////////////// Type Definitions /////////////////////////////////////////
 
@@ -23,19 +40,19 @@ enum axis_enum {
     Z_AXIS
 };
 
+
 ///////////////////// Public Function Prototypes ///////////////////////////////
 
 // Requests and returns acceleration of axis in (1/2)*m/(s^2)
 int16_t accelerometer_read(char axis);
 
-// Returns magnitude of agragate vector in (1/2)*m/(s^2)
-uint16_t acceleration_agg_mag(void);
+// Returns magnitude of aggregate vector in [(1/2)*m/(s^2)]^2 (to avoid square rooting)
+uint32_t acceleration_agg_mag(void);
 
 // Determines whether the rover is right-side-up (true) or upside-down (false).
 bool is_up(void);
 
-// Returns false if the rocket has not launched. Returns true if the rocket has launched.
-// Does not enter function for a substantial amount of time if there are high Gs
-bool is_launched(void);
+// Called by timer0 channel B interrupt. Changes launch_is_a_go global variable
+void is_launched(uint64_t samples);
 
 #endif
