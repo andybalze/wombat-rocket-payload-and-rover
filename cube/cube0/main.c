279b47934fe039516628d7e9b219fa3c37e4de5d
Jordan Johnson <a02293752@usu.edu>
2024-04-28 16:16:32 -0600

    start cube0 main, copied Michael's code

--- cube/cube0/main.c
+++ cube/cube0/main.c
@@ -1,81 +1,273 @@
-/* * * * * * * * * * * * * * *
-      Data Cube Software
- * * * * * * * * * * * * * * */
+////////////////////////////////////////////////////////////////////////////////
+//
+// Data Cube 0 Main
+//
+////////////////////////////////////////////////////////////////////////////////
 
+// Common includes
+#include "uart.h"
+#include "spi.h"
+
+// Cube common includes
+#include "cube_parameters.h"
+#include "digital_io.h"
+#include "trx.h"
+#include "networking_constants.h"
+
+// Specific to this cube includes
+#include "address.h"
+
+// AVR includes
+#include <util/delay.h> // Must come after #include "cube_parameters.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#include "digital_io.h"
-#include "uart.h"
-#include "spi.h"
-#include "trx.h"
+// C Standard Library includes
+#include <stdlib.h>
 
-#define F_CPU 1000000
-#include <util/delay.h>
+/////////////////// Private Defines ///////////////////////////////////////////
 
-#define STARTUP_DELAY_MS (100)
+// If this macro is defined, UART functionality will be included.
+// #define DEBUG_MODE
 
-#define THIS_CUBE_RX_ADDRESS 0x0A0A0A0A
-#define TARGET_RX_ADDRESS THIS_CUBE_RX_ADDRESS
+// The number of milliseconds to wait after the cube is turned on to enter the 
+// LOADING state.
+#define STARTUP_DURATION_MS 1000
+// Wait one full second.
 
-static char* restart_message_format 	= "\n\rHello World.\n\r";
-static char* received_message_format 	= "Received message:\t%s\n\r";
-static char* timeout_message_format		= "Message timed out.\n\r";
+// The number of milliseconds that the data cube's limit switch must be
+// depressed for before the cube enters the LOADED state.
+#define LOADING_DURATION_MS 5000
+// Wait two full seconds (too short?).
 
-static trx_payload_element_t *example_payload1 = "This better work, cuz if not...";
-static trx_payload_element_t *example_payload2 = "I swear I'll shit yourself!!!\n\r";
+// The number of seconds the data cube waits until it turns off its LED.
+#define LOADED_1_DURATION_S 10
 
-static trx_payload_element_t received_payload[TRX_PAYLOAD_LENGTH + 1];
+// The number of milliseconds that the data cube waits after it is dispensed
+// from the rover (its limit switch is released) before beginning its RF
+// operations. During this time, the data cube's RF hardware is brough online.
+#define DISPENSING_DURATION_MS 1000
+// Wait 1/10 seconds for the trx to start up.
+
+#define LED_COLOR_STARTUP               0   // Off
+#define LED_COLOR_READY_TO_LOAD         2   // Green
+#define LED_COLOR_LOADING               2   // Green
+#define LED_COLOR_LOADED_1              4   // Red
+#define LED_COLOR_LOADED_2              0   // Off
+#define LED_COLOR_DISPENSING            1   // Blue
+
+/////////////////// Private Typedefs ///////////////////////////////////////////
+
+// The states that the data cube state machine can be in
+enum data_cube_state_enum {
+
+    // The cube waits in this state for a certain amount of time before moving to
+    // the READY_TO_LOAD state.
+    STARTUP,
+
+    // In this state, the data cube watches for the limit switch be be depressed,
+    // which indicates that the data cube (maybe) is being loaded into the
+    // dispenser.
+    READY_TO_LOAD,
+
+    // In this state, the data cube watches for either 1) the limit switch to be
+    // released, in which case the cube wasn't loaded properly and returns to the
+    // READY_TO_LOAD state, or 2) a certain amount of time passes without the
+    // limit switch being released, which indicates that the data cube has been
+    // loaded into the dispenser.
+    LOADING,
+
+    // The data cube waits in the dispenser to be dispensed, which is signalled
+    // by the limit switch being released.
+    LOADED,
+
+    // The data cube has been dispensed onto the ground and waits there while the
+    // mission hardware (the transceiver) powers up.
+    DISPENSING,
+
+    // The data cube performs its mission functions. Once the data cube enters
+    // this state, it will never leave it until the data cube is switched off.
+    OPERATIONAL
+
+};
+typedef enum data_cube_state_enum data_cube_state_t;
+
+/////////////////// Private Global Variables ///////////////////////////////////
+
+// The current state of this data cube.
+volatile static data_cube_state_t current_state = STARTUP;
+
+/////////////////// Private Function Prototypes ///////////////////////////////
+
+// The code that's run each time around the state machine loop, depending on
+// the state machine's current state. These functions handle any behavior of
+// their states, as well as transitions to any other states.
+void state_code_startup(void);
+void state_code_ready_to_load(void);
+void state_code_loading(void);
+void state_code_loaded(void);
+void state_code_dispensing(void);
+void state_code_operational(void);
+
+// Starts the 16-bit Timer 1 and sets it to run for a certain number of
+// milliseconds. Function can check whether the timer is up by checking the
+// TIFR1 OCF1A bit. The stop_timer function must then be called.
+void start_timer(uint16_t milliseconds);
+
+// Stops the 16-bit Timer 1.
+void stop_timer();
+
+/////////////////// Public Function Bodies ////////////////////////////////////
 
 int main() {
 
-	// Let's wait for all of our hardware to turn on.
-	_delay_ms(STARTUP_DELAY_MS);
+    digital_io_initialize();
+    uart_initialize();
 
-	digital_io_initialize();
-	uart_initialize();
-	uart_transmit_formatted_message(restart_message_format);
-	UART_WAIT_UNTIL_DONE();
+    uart_transmit_formatted_message("\n\r::: Data Cube 0 :::\n\r");
 
-	
-	trx_initialize(THIS_CUBE_RX_ADDRESS);
+    start_timer(STARTUP_DURATION_MS);
+    current_state = STARTUP;
 
-	trx_transmission_outcome_t outcome;
+    // The main state machine loop.
+    while(1) {
+        switch (current_state)
+        {
+        case STARTUP:
+            state_code_startup();
+            break;
 
-	uart_transmit_formatted_message("Attempting to transmit payload 1.\n\r");
-	UART_WAIT_UNTIL_DONE();
-	if (trx_transmit_payload(TARGET_RX_ADDRESS, example_payload1, TRX_PAYLOAD_LENGTH) == TRX_TRANSMISSION_SUCCESS) {
-		uart_transmit_formatted_message("Successfully transmitted payload 1.\n\r");
-	} else {
-		uart_transmit_formatted_message("Failed to transmit payload 1.\n\r");
-	}
-	UART_WAIT_UNTIL_DONE();
+        case READY_TO_LOAD:
+            state_code_ready_to_load();
+            break;
 
-	_delay_ms(1000);
-	
-	uart_transmit_formatted_message("Attempting to transmit payload 2.\n\r");
-	UART_WAIT_UNTIL_DONE();
-	if (trx_transmit_payload(TARGET_RX_ADDRESS, example_payload2, TRX_PAYLOAD_LENGTH) == TRX_TRANSMISSION_SUCCESS) {
-		uart_transmit_formatted_message("Successfully transmitted payload 2.\n\r");
-	} else {
-		uart_transmit_formatted_message("Failed to transmit payload 2.\n\r");
-	}
-	UART_WAIT_UNTIL_DONE();
+        case LOADING:
+            state_code_loading();
+            break;
 
-	while(1) {
+        case LOADED:
+            state_code_loaded();
+            break;
 
-		uart_transmit_formatted_message("Attempting to receive transmissions.\r\n");
-		UART_WAIT_UNTIL_DONE();
-	
-		trx_reception_outcome_t outcome;
-		outcome = trx_receive_payload(received_payload, 1000);
-		if (outcome == TRX_RECEPTION_SUCCESS) {
-			uart_transmit_formatted_message(received_message_format, received_payload);
-		} else {
-			uart_transmit_formatted_message(timeout_message_format);
-		}
-		UART_WAIT_UNTIL_DONE();
+        case DISPENSING:
+            state_code_dispensing();
+            break;
 
-	}
+        case OPERATIONAL:
+            // Since a data cube should never leave the OPERATIONAL state, having an
+            // infinite loop wrap this function call makes things a little faster.
+            while(1) {
+                state_code_operational();
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+
+}
+
+/////////////////// Private Function Bodies ////////////////////////////////////
+
+void state_code_startup(void) {
+
+    if ((TIFR1 & _BV(OCF1A)) != 0) {
+
+#ifdef DEBUG_MODE
+        uart_transmit_formatted_message("Transitioning to READY_TO_LOAD state.\n\r");
+        UART_WAIT_UNTIL_DONE();
+#endif
+
+        stop_timer();
+        current_state = READY_TO_LOAD;
+        LED_set(LED_COLOR_READY_TO_LOAD);
+
+    }
+
+}
+
+void state_code_ready_to_load(void) {
+
+    if (SW_read(SW1)) {
+
+#ifdef DEBUG_MODE
+    uart_transmit_formatted_message("Transitioning to LOADING state.\n\r");
+    UART_WAIT_UNTIL_DONE();
+#endif
+
+        start_timer(LOADING_DURATION_MS);
+        current_state = LOADING;
+        LED_set(LED_COLOR_LOADING);
+
+    }
+
+}
+
+void state_code_loading(void) {
+
+    if (!SW_read(SW1)) {
+
+#ifdef DEBUG_MODE
+        uart_transmit_formatted_message("Transitioning to READY_TO_LOAD state.\n\r");
+        UART_WAIT_UNTIL_DONE();
+#endif
+
+        stop_timer();
+        current_state = READY_TO_LOAD;
+        LED_set(LED_COLOR_READY_TO_LOAD);
+
+    }
+
+    else if ((TIFR1 & _BV(OCF1A)) != 0) {
+
+#ifdef DEBUG_MODE
+        uart_transmit_formatted_message("Transitioning to LOADED state.\n\r");
+        UART_WAIT_UNTIL_DONE();
+#endif
+
+        stop_timer();
+        start_timer(1000);
+        current_state = LOADED;
+        LED_set(LED_COLOR_LOADED_1);
+
+    }
+
+}
+
+void state_code_loaded(void) {
+
+    static int seconds_passed;
+
+    if ((TIFR1 & _BV(OCF1A)) != 0) {
+        stop_timer();
+        seconds_passed = seconds_passed + 1;
+        if (seconds_passed >= LOADED_1_DURATION_S) {
+            LED_set(LED_COLOR_LOADED_2);
+        } else {
+            start_timer(1000);
+        }
+    }
+
+    if (!SW_read(SW1)) {
+        start_timer(DISPENSING_DURATION_MS);
+        current_state = DISPENSING;
+        trx_initialize(MY_DATA_LINK_ADDR);
+        LED_set(LED_COLOR_DISPENSING);
+    }
+
+}
+
+void state_code_dispensing(void) {
+
+    if ((TIFR1 & _BV(OCF1A)) != 0) {
+        stop_timer();
+        current_state = OPERATIONAL;
+        LED_set(0);
+    }
+
+}
+
+void state_code_operational(void) {
 
 }
